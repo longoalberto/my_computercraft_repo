64aaee60c0084609586e2d449852f4d5536fd078
Longo Alberto <albertolongo921@gmail.com>
2020-11-28 12:43:40 +0100

    1

--- sync.lua
+++ sync.lua
@@ -1,5 +1,5 @@
 print("Redownloading program files")
 shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/quarry.lua quarry")
-shell.run("https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/api.lua")
-shell.run("https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/test.lua")
+shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/api.lua api")
+shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/test.lua test")
 
