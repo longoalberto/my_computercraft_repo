9475258329122b0587d04ea7a379fff9f4540aa1
Longo Alberto <albertolongo921@gmail.com>
2020-11-28 13:50:21 +0100

    1

--- sync.lua
+++ sync.lua
@@ -1,8 +1,3 @@
-shell.run("mkdir updater")
-shell.run("mkdir api")
-
-shell.run("rm updater/file_index")
-shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/updater/file_index.lua file_index")
-shell.run("updater/file_index")
-
-shell.run("updater/file_index")
+shell.run("rm file_index")
+shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/file_index.lua file_index")
+shell.run("file_index")
