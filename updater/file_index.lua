ba690a5de60f73430c0e2a0237e84a19994909bb
Longo Alberto <albertolongo921@gmail.com>
2020-11-28 13:34:12 +0100

    1

--- updater/file_index.lua
+++ updater/file_index.lua
@@ -4,7 +4,7 @@ shell.run("rm quarry")
 shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/quarry.lua quarry")
 
 shell.run("rm api")
-shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/api.lua api")
+shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/api/turtle.lua api")
 
 shell.run("rm test")
 shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/test.lua test")
