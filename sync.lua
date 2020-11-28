173fd1627928f20710614c365627c8bf9b8832a6
Longo Alberto <albertolongo921@gmail.com>
2020-11-28 12:58:37 +0100

    1

--- sync.lua
+++ sync.lua
@@ -1,11 +1,16 @@
-print("Redownloading program files")
+if arg[1] == "update" then
+    shell.run("rm sync")
+    shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/sync.lua sync")
+else
+    print("Redownloading program files")
 
-shell.run("rm quarry")
-shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/quarry.lua quarry")
+    shell.run("rm quarry")
+    shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/quarry.lua quarry")
 
-shell.run("rm api")
-shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/api.lua api")
+    shell.run("rm api")
+    shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/api.lua api")
 
-shell.run("rm test")
-shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/test.lua test")
+    shell.run("rm test")
+    shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/test.lua test")
 
+end
--- update_sync.lua
+++
@@ -1,2 +0,0 @@
-shell.run("rm sync")
-shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/sync.lua sync")
