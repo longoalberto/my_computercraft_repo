8aa1f6a3febf5ea2153e29411c23f46fbb3a1de9
Longo Alberto <albertolongo921@gmail.com>
2020-11-28 10:51:35 +0100

    1

--- sync.lua
+++ sync.lua
@@ -1,5 +1,5 @@
 print("Redownloading program files")
-os.call("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/quarry quarry")
+shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/quarry quarry")
 
 print("Resync from repository completed")
 
