b9380b75d4934e2e69c620bea3638fe6d61a7e47
Longo Alberto <albertolongo921@gmail.com>
2020-11-28 13:11:31 +0100

    1

---
+++ file_index.lua
@@ -0,0 +1,10 @@
+print("Redownloading program files")
+
+shell.run("rm quarry")
+shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/quarry.lua quarry")
+
+shell.run("rm api")
+shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/api.lua api")
+
+shell.run("rm test")
+shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/test.lua test")
--- sync.lua
+++ sync.lua
@@ -1,17 +1,4 @@
-if arg[1] == "update" then
-    shell.run("rm sync")
-    shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/sync.lua sync")
-    shell.run("sync")
-else
-    print("Redownloading program files")
 
-    shell.run("rm quarry")
-    shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/quarry.lua quarry")
-
-    shell.run("rm api")
-    shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/api.lua api")
-
-    shell.run("rm test")
-    shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/test.lua test")
-
-end
+shell.run("rm file_index")
+shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/file_index.lua file_index")
+shell.run("file_index")
