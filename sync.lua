c152f9fbf514602354395bc6b5f4a494ddd0f48c
Longo Alberto <albertolongo921@gmail.com>
2020-11-28 12:39:56 +0100

    1

--- sync.lua
+++ sync.lua
@@ -1,7 +1,5 @@
-print("Removing previous sync file")
-shell.run("rm sync")
-
 print("Redownloading program files")
 shell.run("wget https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/quarry.lua quarry")
-
+shell.run("https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/api.lua")
+shell.run("https://raw.githubusercontent.com/longoalberto/my_computercraft_repo/main/test.lua")
 
